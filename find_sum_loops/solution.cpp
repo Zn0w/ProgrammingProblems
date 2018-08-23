fa26034f3e5ede93cf8e290b19f749f20574945e
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-08-23 20:38:51 +0300

    added solution for problem 1

--- find_sum_loops/solution.cpp
+++ find_sum_loops/solution.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 
+int arr[10] = {12, 44, 58, 3, 5, 6, 90, 23, 43, 71}; // The sum of those numbers is 355
+
+bool sumFor();
+bool sumWhile();
+bool sumRecursion(int, int);
+
 main()
 {
-	std::cout << "Hello World!" << std::endl;
-	system("pause");
+	if (sumFor() && sumWhile() && sumRecursion(0, 0))
+		std::cout << "Solution is correct!" << std::endl;
+	else
+		std::cout << "Solution is incorrect!" << std::endl;
+
+	system("pause");			
+}
+
+bool sumFor()
+{
+	int sum = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		sum += arr[i];
+	}
+
+	return sum == 355;
+}
+
+bool sumWhile()
+{
+	int sum = 0;
+
+	int i = 0;
+	while (i < 10)
+	{
+		sum += arr[i];
+		i++;
+	}
+
+	return sum == 355;
+}
+
+bool sumRecursion(int sum, int i)
+{
+	sum += arr[i];
+	if (i < 10)
+	{
+		i++;
+		sumRecursion(sum, i);
+	}
+	else
+		return sum == 355;
 }
